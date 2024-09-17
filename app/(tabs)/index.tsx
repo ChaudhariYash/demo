bf18b45168b4a84949634274e1b897db950adee0
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-17 16:38:57 +0530

    using useState in function lerning

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Text, View } from "react-native";
 
 const App = () => {
- const  fruit=(val)=>
-  {
-    console.warn(val)
-  }
+ const [name,setName]=useState("Yash")
+ function testname(){
+  setName("sagar")
+ }
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>Button and Press Event </Text>
-      <Button title='On Press' onPress={fruit} color={'red'} />
-
-      <Button title='On Press2' onPress={()=>fruit("Hello Yash")} color={'black'} />
-
+      <Text style={{ fontSize: 30 }}>Usestate  </Text>
+       <Text style={{fontSize:20 ,textAlign:'center' }}>{name} </Text>
+       <Button title="On Press" onPress={testname}/>
     </View>
   );
 };
