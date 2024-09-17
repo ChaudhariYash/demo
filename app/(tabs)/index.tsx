ba1bb3bb8c9e661ab1955dc4039f44ee5cd6988f
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-17 14:55:31 +0530

    jsx using learning

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import {Button, Text,View} from 'react-native';
 
+
+const name = "Yash" ;
+let age = 8 ;
+function fruit()
+{
+  return 'Apple';
+}
+
+
+
 const App = () => 
 {
   return(
     <View>
-      <Text style={{fontSize:30}}> Hello Word </Text>
-      <Text style={{fontSize:30}}> Hello secong text componenet  </Text>
+      <Text style={{fontSize:30}}> Hello Word</Text>
+      <Text style={{fontSize:30}}> {fruit()}  </Text>
+      <Text style={{fontSize:30}}> {name}  </Text>
+      <Text style={{fontSize:30}}> {age}  </Text>
+      <Text style={{fontSize:30}}> {100*2}  </Text>
+      <Text style={{fontSize:30}}> {age===39?"above 30":"unknow age"}</Text>
+
+    
       <Button title ='Press Here' />
 
     </View>
