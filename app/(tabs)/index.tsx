969dd9993fb790bb1ae6bb8dcf4aede64be9ec0f
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-17 16:57:03 +0530

    porps learning

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,15 +2,22 @@ import React, { useState } from "react";
 import { Button, Text, View } from "react-native";
 
 const App = () => {
- const [name,setName]=useState("Yash")
- function testname(){
-  setName("sagar")
- }
+ const[name,setName]=useState("tipu")
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>Usestate  </Text>
-       <Text style={{fontSize:20 ,textAlign:'center' }}>{name} </Text>
-       <Button title="On Press" onPress={testname}/>
+      <Text style={{ fontSize: 30 }}>Props in react js </Text>
+      <Button title=" update props"  onPress={()=>setName("chaudhari")}/>
+      <User name={name} age ={23}/>
+    </View>
+  );
+};
+
+const User = (props) => {
+ 
+  return (
+    <View style={{backgroundColor:'green', padding:5}}>
+      <Text style={{ fontSize: 30 }}> {props.name}</Text>
+      <Text style={{ fontSize: 30 }}> {props.age}</Text>
     </View>
   );
 };
