9b9169d47fbd14a78a843a233daeec8ed17624d5
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-18 20:08:51 +0530

    TextInpute handle using useState

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,34 @@
-import React from "react";
-import { Text, View ,StyleSheet} from "react-native";
+import React, { useState } from "react";
+import { Text, View,TextInput,StyleSheet, Button} from "react-native";
 
 const App = () => {
+
+  const [name,setName] = useState("Yash")
   return (
-    <View style={{flex:1,backgroundColor:'white',paddingTop:200}}>
-      <Text style={{ fontSize: 30 }}>style in react native</Text>
-      <Text style={Styles.textbox}>style in react native</Text>
+    <View>
+      <Text style={{ fontSize: 30 }}>Handle text input </Text>
+      <Text style={{ fontSize: 30 }}>Your name is:{name}</Text>
+      <TextInput 
+       style = {styles.Textinput}
+      placeholder="Enter Your Name" 
+      value={name}
+      onChangeText={(text)=>setName(text)}
+       />
+       <Button title='clear Input Value' onPress={()=>setName('')}/>
     </View>
   );
 };
 
-
-const Styles = StyleSheet.create 
-({
-  textbox:
+const styles = StyleSheet.create({
+  Textinput:
   {
-      fontSize:30,
-      padding:5,
-      backgroundColor:'lightblue',
-      color:'black',
-      margin:5,
-      borderRadius:6,
-      borderColor:"red",
-      borderWidth:2,
-      textAlign:'center',
-
+    fontSize:18,
+    color:'red',
+    borderWidth:2,
+    borderRadius:2,
+    borderColor:'black',
+    margin:10
   },
 })
+
 export default App;
