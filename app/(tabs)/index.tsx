265e09f0585ece0be1db04b8976834c46229545c
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-19 18:04:49 +0530

    Simple from react native

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,98 @@
 import React, { useState } from "react";
-import { Text, View,TextInput,StyleSheet, Button} from "react-native";
+import { Text, View, TextInput, StyleSheet, Button } from "react-native";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [display, setDisplay] = useState(false);
 
-  const [name,setName] = useState("Yash")
+  const resetFromData=()=>
+  {
+    setDisplay(false);
+    setEmail("");
+    setName("");
+    setPassword("");
+  }
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>Handle text input </Text>
-      <Text style={{ fontSize: 30 }}>Your name is:{name}</Text>
-      <TextInput 
-       style = {styles.Textinput}
-      placeholder="Enter Your Name" 
-      value={name}
-      onChangeText={(text)=>setName(text)}
-       />
-       <Button title='clear Input Value' onPress={()=>setName('')}/>
+      <Text style={{ fontSize: 30, marginTop: 150, textAlign: "center" }}>
+        # Simple From In React-Native #
+      </Text>
+      <TextInput
+        style={styles.textinput}
+        placeholder="Enter Your Name"
+        onChangeText={(text) => setName(text)}
+        value={name}
+      />
+
+      <TextInput
+        style={styles.textinput}
+        placeholder="Enter Your Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+
+      <TextInput
+        style={styles.textinput}
+        placeholder="Enter Your Password "
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+      <View style={styles.buttonprint}>
+        <Button
+          color="white"
+          title="print Value"
+          onPress={() => setDisplay(true)}
+        />
+      </View>
+      <View style={styles.buttonclear}>
+        <Button color="white" title="Clear Value" onPress={resetFromData} />
+      </View>
+      <View>
+        {display ? (
+          <View>
+            <Text style={{ fontSize: 20 }}>User name is :{name}</Text>
+            <Text style={{ fontSize: 20 }}>User email is :{email}</Text>
+            <Text style={{ fontSize: 20 }}>User Password is :{password}</Text>
+          </View>
+        ) : null}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  Textinput:
-  {
-    fontSize:18,
-    color:'red',
-    borderWidth:2,
-    borderRadius:2,
-    borderColor:'black',
-    margin:10
+  textinput: {
+    fontSize: 18,
+    color: "blue",
+    borderWidth: 1,
+    borderColor: "black",
+    margin: 10,
+    padding: 10,
+    marginTop: 50,
   },
-})
+  buttonprint: {
+    backgroundColor: "green",
 
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 50,
+
+    borderWidth: 1,
+    borderRadius: 222,
+  },
+  buttonclear: {
+    backgroundColor: "red",
+    color: "red",
+
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 5,
+
+    borderWidth: 1,
+    borderRadius: 222,
+  },
+});
 export default App;
