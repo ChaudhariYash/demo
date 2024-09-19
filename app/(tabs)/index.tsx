e2277db2ebdaaa59d3d1462aa63e4465ef551184
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-19 19:13:24 +0530

    learning Flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,59 @@
 import React, { useState } from "react";
-import { Text, View, TextInput, StyleSheet, Button } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  Button,
+  FlatList,
+} from "react-native";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [display, setDisplay] = useState(false);
-
-  const resetFromData=()=>
-  {
-    setDisplay(false);
-    setEmail("");
-    setName("");
-    setPassword("");
-  }
+  const users = [
+    {
+      id: 1,
+      name: "yash",
+    },
+    {
+      id: 2,
+      name: "Tipu",
+    },
+    {
+      id: 3,
+      name: "Sai",
+    },
+    {
+      id: 4,
+      name: "Aditya",
+    },
+  ]
   return (
     <View>
-      <Text style={{ fontSize: 30, marginTop: 150, textAlign: "center" }}>
-        # Simple From In React-Native #
+      <Text style={{ fontSize: 30, marginTop: 30, textAlign: "center" }}>
+        List with Flatlist Component{" "}
       </Text>
-      <TextInput
-        style={styles.textinput}
-        placeholder="Enter Your Name"
-        onChangeText={(text) => setName(text)}
-        value={name}
+      <FlatList
+        data={users}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>)}
+          keyExtractor={item=>item.id}
       />
-
-      <TextInput
-        style={styles.textinput}
-        placeholder="Enter Your Email"
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-      />
-
-      <TextInput
-        style={styles.textinput}
-        placeholder="Enter Your Password "
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-      />
-      <View style={styles.buttonprint}>
-        <Button
-          color="white"
-          title="print Value"
-          onPress={() => setDisplay(true)}
-        />
-      </View>
-      <View style={styles.buttonclear}>
-        <Button color="white" title="Clear Value" onPress={resetFromData} />
-      </View>
-      <View>
-        {display ? (
-          <View>
-            <Text style={{ fontSize: 20 }}>User name is :{name}</Text>
-            <Text style={{ fontSize: 20 }}>User email is :{email}</Text>
-            <Text style={{ fontSize: 20 }}>User Password is :{password}</Text>
-          </View>
-        ) : null}
-      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  textinput: {
-    fontSize: 18,
-    color: "blue",
-    borderWidth: 1,
-    borderColor: "black",
-    margin: 10,
-    padding: 10,
-    marginTop: 50,
+const styles = StyleSheet.create
+({
+  item:
+  {
+    fontSize:20,
+    padding:10,
+    color:"white",
+    backgroundColor:"green",
+    borderColor:"black",
+    borderWidth:1,
+    margin:10
   },
-  buttonprint: {
-    backgroundColor: "green",
+})
 
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 50,
-
-    borderWidth: 1,
-    borderRadius: 222,
-  },
-  buttonclear: {
-    backgroundColor: "red",
-    color: "red",
-
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 5,
-
-    borderWidth: 1,
-    borderRadius: 222,
-  },
-});
 export default App;
