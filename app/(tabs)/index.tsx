72102436747a3d735831919c6fb434f19cfc21ca
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-20 12:00:56 +0530

    map function and scrollview learning

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,9 @@ import React, { useState } from "react";
 import {
   Text,
   View,
-  TextInput,
+  ScrollView,
   StyleSheet,
-  Button,
-  FlatList,
+
 } from "react-native";
 
 const App = () => {
@@ -24,36 +23,78 @@ const App = () => {
     },
     {
       id: 4,
+      name: "darshan",
+    },
+    {
+      id: 5,
       name: "Aditya",
     },
-  ]
-  return (
-    <View>
-      <Text style={{ fontSize: 30, marginTop: 30, textAlign: "center" }}>
-        List with Flatlist Component{" "}
-      </Text>
-      <FlatList
-        data={users}
-        renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>)}
-          keyExtractor={item=>item.id}
-      />
-    </View>
-  );
+    {
+      id: 6,
+      name: "ganesh",
+    },
+    {
+      id: 7,
+      name: "Samya",
+    },
+    {
+      id: 8,
+      name: "Pankya",
+    },
+    {
+      id: 9,
+      name: "jayesh",
+    },
+    {
+      id: 10,
+      name: "pranav",
+    },
+    {
+      id: 11,
+      name: "Ameya",
+    },
+    {
+      id: 12,
+      name: "harshal",
+    },
+    {
+      id: 13,
+      name: "dipesh",
+    },
+    {
+      id: 14,
+      name: "Ranjeet",
+    },
+    {
+      id: 15,
+      name: "lalit",
+    },
+    {
+      id: 16,
+      name: "Vinay",
+    },
+    
+  ];
+  return <View>
+    <Text style={{fontSize:30}}> List with map Function</Text>
+    <ScrollView>
+    {
+      users.map((item)=><Text style={styles.item}>{item.name}</Text>)
+    }
+   </ScrollView>
+  </View>;
 };
 
-const styles = StyleSheet.create
-({
-  item:
-  {
-    fontSize:20,
-    padding:10,
-    color:"white",
-    backgroundColor:"green",
-    borderColor:"black",
-    borderWidth:1,
-    margin:10
+const styles = StyleSheet.create({
+  item: {
+    fontSize: 20,
+    padding: 10,
+    color: "white",
+    backgroundColor: "green",
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 10,
   },
-})
+});
 
 export default App;
