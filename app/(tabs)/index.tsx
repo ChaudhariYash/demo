2b410a0f139b7b68d44cdc61b6d2edc6e0d449dd
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-20 18:19:39 +0530

    class components using

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,40 +1,20 @@
-import React from "react";
-import { Text, View,  SectionList } from "react-native";
+import React ,{Component} from 'react' ;
+import {Button,Text,TextInput,View} from 'react-native';
 
-const App = () => {
-  const users = [
-    {
-      id: 1,
-      name: "yash",
-      Data:["php","react-js","css"]
-    },
-    {
-      id: 2,
-      name: "Tipu",
-      Data:["java","php","python"]
-    },
-    {
-      id: 3,
-      name: "Sai",
-      Data:["Css","html","Java-script"]
-    },
-    {
-      id: 4,
-      name: "darshan",
-      Data:["C++","angura","python"]
-    },
-  ];
-return (
-  <View>
-    <Text style={{fontSize:31}}>Selection List In React-Native  </Text>
-    <SectionList 
-      sections={users}
-      renderItem={({item})=><Text style={{fontSize:30}}>{item}</Text>}
-      renderSectionHeader={({section:{name}})=>(
-        <Text style ={{fontSize:35}}>{name} </Text>
- ) }
-  />
-  </View>
-)}
-
-export default App;
+class Student extends Component {
+  fruit =()=>
+  {
+    console.warn("apple");
+  }
+  render()
+  {
+    return(
+      <View>
+        <Text style={{fontSize:30,color:"green"}}>student component </Text>
+       <TextInput placeholder='enter your name' />
+       <Button title='press me ' onPress={this.fruit} />
+       <Student />
+      </View>
+    )
+  }
+}
