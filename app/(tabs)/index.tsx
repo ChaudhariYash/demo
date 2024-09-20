92189831a7c91cdeda266c7720a910d71112b54a
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-20 17:22:24 +0530

    Component in loop with flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,80 +1,70 @@
 import React, { useState } from "react";
-import {
-  Text,
-  View,
-  ScrollView,
-  StyleSheet,
-
-} from "react-native";
+import { Text, View, StyleSheet,FlatList } from "react-native";
 
 const App = () => {
   const users = [
     {
       id: 1,
       name: "yash",
+      email:"yashe@rtyjng",
     },
     {
       id: 2,
       name: "Tipu",
+      email:"tipu@rtyjng",
     },
     {
       id: 3,
       name: "Sai",
+      email:"saie@rtyjng",
     },
     {
       id: 4,
       name: "darshan",
+      email:"darshan@rtyjng",
     },
-    {
-      id: 5,
-      name: "Aditya",
-    },
-    {
-      id: 6,
-      name: "ganesh",
-    },
-    {
-      id: 7,
-      name: "Samya",
-    },
-    {
-      id: 8,
-      name: "Pankya",
-    },
-    {
-      id: 9,
-      name: "jayesh",
-    },
-    {
-      id: 10,
-      name: "pranav",
-    },
-   
-    
   ];
-  return <View>
-    <Text style={{fontSize:30}}>Grid with dynamic data</Text>
-   <View style={{flex:1,flexDirection:"row",flexWrap:"wrap"}}>
-    {
-      users.map((item)=><Text style={styles.item}>{item.name}</Text>)
-    }
-   </View>
-  </View>;
+  return (
+    <View>
+      <Text style={{ fontSize: 30,marginTop:200,marginBottom:50 }}>Component in loop with flatlist </Text>
+      <FlatList 
+        data={users}
+        renderItem={({item})=> <Userdate item= {item}/> }
+      />
+    </View>
+  );
 };
+
+const Userdate =(props)=>
+{
+  const item = props.item
+  return(
+    <View style={styles.box}>
+          
+    <Text style={styles.item}>{item.name}</Text>
+   <Text style={styles.item}>{item.email}</Text>
+ 
+ </View>
+  )
+}
 
 const styles = StyleSheet.create({
   item: {
-    fontSize:25,
-    backgroundColor:"blue",
-    color:"white",
-    margin:5,
+    fontSize: 25,
+    color: "Orange",
+    margin: 2,
+    padding: 2,
+    flex:1,
+    backgroundColor:"skyblue",
+    textAlign:"center",
+  },
+  box:
+  {
+    flexDirection:'row',
+    borderWidth:2,
+    borderColor:"black",
     padding:5,
-    width:120,
-    height:120,
-   textAlignVertical:'center',
-   textAlign:'center',
-
-
+    marginBottom:50,
   },
 });
 
