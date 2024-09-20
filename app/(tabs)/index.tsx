dc7878a2e618867e4d9860317d667771b95b581f
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-20 17:58:58 +0530

    Selection List In React-Native

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,71 +1,40 @@
-import React, { useState } from "react";
-import { Text, View, StyleSheet,FlatList } from "react-native";
+import React from "react";
+import { Text, View,  SectionList } from "react-native";
 
 const App = () => {
   const users = [
     {
       id: 1,
       name: "yash",
-      email:"yashe@rtyjng",
+      Data:["php","react-js","css"]
     },
     {
       id: 2,
       name: "Tipu",
-      email:"tipu@rtyjng",
+      Data:["java","php","python"]
     },
     {
       id: 3,
       name: "Sai",
-      email:"saie@rtyjng",
+      Data:["Css","html","Java-script"]
     },
     {
       id: 4,
       name: "darshan",
-      email:"darshan@rtyjng",
+      Data:["C++","angura","python"]
     },
   ];
-  return (
-    <View>
-      <Text style={{ fontSize: 30,marginTop:200,marginBottom:50 }}>Component in loop with flatlist </Text>
-      <FlatList 
-        data={users}
-        renderItem={({item})=> <Userdate item= {item}/> }
-      />
-    </View>
-  );
-};
-
-const Userdate =(props)=>
-{
-  const item = props.item
-  return(
-    <View style={styles.box}>
-          
-    <Text style={styles.item}>{item.name}</Text>
-   <Text style={styles.item}>{item.email}</Text>
- 
- </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  item: {
-    fontSize: 25,
-    color: "Orange",
-    margin: 2,
-    padding: 2,
-    flex:1,
-    backgroundColor:"skyblue",
-    textAlign:"center",
-  },
-  box:
-  {
-    flexDirection:'row',
-    borderWidth:2,
-    borderColor:"black",
-    padding:5,
-    marginBottom:50,
-  },
-});
+return (
+  <View>
+    <Text style={{fontSize:31}}>Selection List In React-Native  </Text>
+    <SectionList 
+      sections={users}
+      renderItem={({item})=><Text style={{fontSize:30}}>{item}</Text>}
+      renderSectionHeader={({section:{name}})=>(
+        <Text style ={{fontSize:35}}>{name} </Text>
+ ) }
+  />
+  </View>
+)}
 
 export default App;
