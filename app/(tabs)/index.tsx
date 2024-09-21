5c0a49116719fce6686d20fc9f26fb8b144a59bd
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-21 13:30:16 +0530

    state and props in class component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,19 +2,31 @@ import React ,{Component} from 'react' ;
 import {Button,Text,TextInput,View} from 'react-native';
 
 class Student extends Component {
-  fruit =()=>
-  {
-    console.warn("apple");
+ constructor() {
+  super();
+  this.state={
+    name:"Yash",
+  
   }
+ }
+
+ updateName(val)
+ {
+   this.setState({name:val})
+ }
   render()
   {
     return(
       <View>
-        <Text style={{fontSize:30,color:"green"}}>student component </Text>
-       <TextInput placeholder='enter your name' />
-       <Button title='press me ' onPress={this.fruit} />
+        <Text style={{fontSize:30,color:"green"}}>{this.state.name} </Text>
+       <TextInput placeholder='enter your name'
+        onChangeText={(text)=>this.updateName(text)}
+         />
+       <Button title='press me ' />
        <Student />
       </View>
     )
   }
 }
+
+export default Student ; 
