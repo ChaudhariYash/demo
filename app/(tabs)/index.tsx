086d864b126fbba38f628b8d5b1aa586fdb43760
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-22 17:20:51 +0530

    Show /Hide Component toggel

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,48 +1,27 @@
 import React, { useEffect, useState } from "react";
 import {Button, Text, View } from "react-native";
 
-const App = () => {
- const [count,setCount]=useState(0);
- const [data,setData]=useState(100);
-
- useEffect(()=>{
-  console.warn("Do some Animation ")
- },[count]);
-
- useEffect(()=>{
-  console.warn("call some api here ")
- },[data]);
-
-  return (
-    <View  style={{marginTop:100}}>
-      <Text style={{fontSize:26}}>{data}  useEffect as ComponentDidMount {count}</Text>
-      <Button title="udate Counter" onPress={()=>setCount (count+1)}  />
-      <Button title="udate Data" onPress={()=>setData (data+1)}  />  
-        <User  info={{data,count}}/>
-   </View>
-  )
-    };
-
-const User=(props)=>
-{ 
- console.warn(props.info)
-  useEffect(()=>
-  {
-    console.warn("run this code when data props is update");
-  },[props.info.data])
-
-  useEffect(()=>
-    {
-      console.warn("run this code when data props is update");
-    },[props.info.data])
-    
-  return (
-    <View  style={{marginTop:100}}>
-      <Text style={{fontSize:26, color:"orange"}}>data:{props.info.data}</Text>
-      <Text style={{fontSize:26, color:"orange"}}>count:{props.info.count}</Text>
-
-   </View>
+const App = () =>
+{const [show,setShow]=useState(false)
+  return(
+    <View>
+      <Text style={{fontSize:40,marginTop:50}}> Show /Hide Component  </Text>
+      <Button title="toggle  Component " onPress={()=>setShow(! show )}/>
+      {/* <Button title="Hide Component " onPress={()=>setShow(false)}/> */}
+        {
+          show  ? <User/> : null 
+        }
+      
+    </View>
   )
 }
 
+const User =()=>
+{ 
+  return(
+  <View>
+    <Text style={{fontSize:30,color:"green"}}>USer Component </Text>
+  </View>
+  )
+}
 export default App;
