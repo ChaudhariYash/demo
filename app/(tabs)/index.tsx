4fc750a4f3fb4de83eaa7a9ca517769fb9870c1f
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-22 15:06:33 +0530

    useEffect lifecycle method component did mount

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,18 @@
-import React ,{Component} from 'react' ;
-import {Button,Text,TextInput,View} from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
 
-class Student extends Component {
- constructor() {
-  super();
-  this.state={
-    name:"Yash",
-  
-  }
- }
+const App = () => {
+  const [count,setCount]=useState(0)
+  useEffect (()=>{
+    console.warn("Hello");
+  },[])
 
- updateName(val)
- {
-   this.setState({name:val})
- }
-  render()
-  {
-    return(
-      <View>
-        <Text style={{fontSize:30,color:"green"}}>{this.state.name} </Text>
-       <TextInput placeholder='enter your name'
-        onChangeText={(text)=>this.updateName(text)}
-         />
-       <Button title='press me ' />
-       <Student />
-      </View>
-    )
-  }
-}
+  return (
+    <View>
+      <Text>hello Word</Text>
+      <Button title='updateCount' onPress={()=>setCount(2)}/>
+    </View>
+  );
+};
 
-export default Student ; 
+export default App;
