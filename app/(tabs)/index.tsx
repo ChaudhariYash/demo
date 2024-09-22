bd391bc0683f5d0fd5a387ecd3dda9e286b684ea
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-22 15:57:45 +0530

    componenet did Update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { Button, Text, View } from "react-native";
+import {Button, Text, View } from "react-native";
 
 const App = () => {
-  const [count,setCount]=useState(0)
-  useEffect (()=>{
-    console.warn("Hello");
-  },[])
+ const [count,setCount]=useState(0);
+ const [data,setData]=useState(100);
+
+ useEffect(()=>{
+  console.warn("Do some Animation ")
+ },[count]);
+
+ useEffect(()=>{
+  console.warn("call some api here ")
+ },[data]);
 
   return (
-    <View>
-      <Text>hello Word</Text>
-      <Button title='updateCount' onPress={()=>setCount(2)}/>
-    </View>
-  );
-};
+    <View  style={{marginTop:100}}>
+      <Text style={{fontSize:26}}>{data}  useEffect as ComponentDidMount {count}</Text>
+      <Button title="udate Counter" onPress={()=>setCount (count+1)}  />
+      <Button title="udate Data" onPress={()=>setData (data+1)}  />  
+   </View>
+  )
+    };
+
+
 
 export default App;
