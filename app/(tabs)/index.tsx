e83497df2fdb467a686b04b858c113c871f0efae
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-22 17:06:50 +0530

    componentdidmount in props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,10 +18,31 @@ const App = () => {
       <Text style={{fontSize:26}}>{data}  useEffect as ComponentDidMount {count}</Text>
       <Button title="udate Counter" onPress={()=>setCount (count+1)}  />
       <Button title="udate Data" onPress={()=>setData (data+1)}  />  
+        <User  info={{data,count}}/>
    </View>
   )
     };
 
+const User=(props)=>
+{ 
+ console.warn(props.info)
+  useEffect(()=>
+  {
+    console.warn("run this code when data props is update");
+  },[props.info.data])
 
+  useEffect(()=>
+    {
+      console.warn("run this code when data props is update");
+    },[props.info.data])
+    
+  return (
+    <View  style={{marginTop:100}}>
+      <Text style={{fontSize:26, color:"orange"}}>data:{props.info.data}</Text>
+      <Text style={{fontSize:26, color:"orange"}}>count:{props.info.count}</Text>
+
+   </View>
+  )
+}
 
 export default App;
