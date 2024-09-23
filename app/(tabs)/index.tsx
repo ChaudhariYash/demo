94ea5a4087e215b10cceeeb6d9a4cbc5d0a01fe1
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-23 20:43:55 +0530

    flex

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,17 @@
 import React, { useEffect, useState } from "react";
 import {Button, Text, View } from "react-native";
 
-const App = () =>
-{const [show,setShow]=useState(false)
+const App = () =>{
+ 
   return(
     <View>
-      <Text style={{fontSize:40,marginTop:50}}> Show /Hide Component  </Text>
-      <Button title="toggle  Component " onPress={()=>setShow(! show )}/>
-      {/* <Button title="Hide Component " onPress={()=>setShow(false)}/> */}
-        {
-          show  ? <User/> : null 
-        }
-      
+      <Text style={{fontSize:40,marginTop:50}}></Text>
+     
     </View>
   )
 }
 
-const User =()=>
-{ 
-  return(
-  <View>
-    <Text style={{fontSize:30,color:"green"}}>USer Component </Text>
-  </View>
-  )
-}
+
+
 export default App;
+  
