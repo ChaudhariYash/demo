c4075111731ca27ae6d5a688514990c6ac0bc249
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-26 18:43:28 +0530

    cleare All

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,12 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Button, TextInput, Modal } from "react-native";
+import { Text, View } from "react-native";
 
-const App = () => { 
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
-  const [showModal, setShowModal] = useState(false); 
-
-  const data = () => {
-    setShowModal(true); 
-  };
-
+const App = () => {
   return (
-    <View style={styles.main}>
-    <Text>hello word</Text>
+    <View>
+      <Text>hello word</Text>
     </View>
   );
 };
-
-
 
 export default App;
