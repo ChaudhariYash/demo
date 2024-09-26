3b02aa2f204ab3ab4e7242b491e2d4ee6b32d514
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-26 18:42:52 +0530

    clear all

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,66 +1,22 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Button, TextInput } from "react-native";
+import { Text, View, StyleSheet, Button, TextInput, Modal } from "react-native";
 
 const App = () => { 
-  const [email,setEmail]=useState(" ")
-  const [pass,setPass]=useState(" ")
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [showModal, setShowModal] = useState(false); 
+
   const data = () => {
-    console.warn({email} ,{pass});
-    
+    setShowModal(true); 
   };
+
   return (
     <View style={styles.main}>
-      <Text style={styles.Login}> Login </Text>
-
-      <TextInput style={styles.placeholder} 
-                 placeholder=" Email / Mobile " 
-                 onChangeText={(text)=>setEmail(text)}/>
-
-      <TextInput
-        style={styles.placeholder}
-        placeholder=" Password"
-        secureTextEntry={true}
-        onChangeText={(text)=>setPass(text)}
-      />
-      <View style={styles.button}>
-        <Button
-          title="Submit"
-          onPress={() => {
-            data();
-          }}
-          color="white"
-        />
-      </View>
+    <Text>hello word</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-  },
-  Login: {
-    fontSize: 30,
-    marginTop: 250,
-    textAlign: "center",
-    fontWeight: "bold",
-    marginBottom: 35,
-  },
-  button: {
-    backgroundColor: "skyblue",
-    margin: 100,
-    marginTop: 10,
-    borderRadius: 56,
-    padding: 10,
-    textAlign: "center",
-  },
-  placeholder: {
-    padding: 15,
-    marginRight: 10,
-    marginLeft: 10,
-    borderWidth: 1,
-    marginTop: 10,
-  },
-});
+
 
 export default App;
