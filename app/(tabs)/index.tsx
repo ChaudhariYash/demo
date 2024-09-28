700afcb17b1fa885ce950f5ddb82c7e6b4baefab
ChaudhariYash <yashchaudhari887@gmail.com>
2024-09-28 18:19:10 +0530

    ActivityIndicator  & Animating

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,90 +1,36 @@
 import React, { useState } from "react";
-import { Text, View ,StyleSheet ,TouchableOpacity } from "react-native";
+import { Text, View,StyleSheet,ActivityIndicator, Button} from "react-native";
 
 const App = () => {
-   const skills =[
-    { 
-      id:1,
-      name:"JAVA",
-    },
-    { 
-      id:2,
-      name:"PHP",
-    },
-    { 
-      id:3,
-      name:"JAVA -SCRIPT",
-    },
-    { 
-      id:4,
-      name:"REACT-NATIVE",
-    },
-    { 
-      id:5,
-      name:"C++",
-    },
-  ]
-  const [radio,setRadio]=useState(1);
-  return (
-    <View   style={Styles.main}>
-       {
-       skills.map((item,index)=>  <TouchableOpacity 
-       key={index} 
-       onPress={()=>setRadio(item.id)}>
-       <View style={Styles.radioWrapper}>
-         <View style={Styles.radio}>
+const [show,setShow]=useState(false);
+  
+ const  display = ()=>
+  {
+      setShow(true);
+      setTimeout(()=>
         {
-         radio === item.id  ?   <View style={Styles.radiobg}></View> :null 
-        }
-         </View>
-     <Text style={Styles.radioText}>{item.name} </Text>
-     </View>
-     </TouchableOpacity> )
-       }
+            setShow(false)
+        },2000);
+  }
+
+  return (
+    <View style={styles.main}>
+      <ActivityIndicator size="large" color="green" animating={show}/>
+      <Button title="Show Loader" onPress={display} />
     </View>
   );
-};   
+}; 
 
-const Styles =StyleSheet.create({
-  main:{
-     flex:1,
-     textAlign:"center",
-     justifyContent:"center",
+const styles = StyleSheet.create
+({
+  main:
+  {
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
     
- },
- radioText:
- {  
-  textAlign:"center",
-  fontSize:30,
-  color :"red",
- 
- },
- radio:
- { 
-  alignItems:"center",
-  height:40,
-  width:40,
-  borderColor:"black",
-  borderWidth:2,
-  borderRadius:20,
-  margin:10,
-  marginLeft:18,
+  },
+})
 
- }, 
- radioWrapper:
- {
-  flexDirection:"row",
-  alignItems:"center",
-  marginLeft:110
- },
- radiobg:
- {
-  backgroundColor:"black",
-  height:29,
-  width:29,
-  borderRadius:19,
-  margin:4
- },
-} )
 
 export default App;
