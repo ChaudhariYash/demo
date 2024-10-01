d607aaf0dbf2b807fd15930e6ead741edf0156a9
ChaudhariYash <yashchaudhari887@gmail.com>
2024-10-01 13:16:56 +0530

    install npm package and  use web view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,12 @@
-import React, { useState } from "react";
-import { Text, View, StyleSheet, Platform } from "react-native";
+import React  from "react";
+import{} from "react-native";
+import {WebView} from "react-native-webview"; 
 
 const App = () => {
   return (
-    <View style={styles.main}>
-      <Text style={{ fontSize: 30 }}> Platfrom : {Platform.OS}</Text>
-      { 
-        Platform.OS=="android"?  
-        <View style={{ backgroundColor: "red", height: 100, width: 100 }}>  </View>
-         :
-         <View style={{ backgroundColor: "green", height: 100, width: 100 }}></View>
-      }
+    <WebView source={{uri:"https://docs.expo.dev/versions/latest/sdk/webview/"}}/>
+  )
+}
 
-      <Text style= {styles.text}>Hello</Text>
-      <Text style= {styles.text}>{JSON.stringify(Platform.constants.reactNativeVersion.minor)}</Text>
-    </View>
-  );
-};
 
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    justifyContent:"center"
-  },
-  text:
-  {
-    color:Platform.OS=="android"?"orange":"blue" ,
-    fontSize:"30" ,
-  },
-});
 export default App;
