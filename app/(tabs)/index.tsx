7d1606c9fc90d480d2f4258916059bd6c57a0c01
ChaudhariYash <yashchaudhari887@gmail.com>
2024-10-01 13:04:09 +0530

    platfrom using ios and android

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Button,StatusBar} from "react-native";
+import { Text, View, StyleSheet, Platform } from "react-native";
 
 const App = () => {
+  return (
+    <View style={styles.main}>
+      <Text style={{ fontSize: 30 }}> Platfrom : {Platform.OS}</Text>
+      { 
+        Platform.OS=="android"?  
+        <View style={{ backgroundColor: "red", height: 100, width: 100 }}>  </View>
+         :
+         <View style={{ backgroundColor: "green", height: 100, width: 100 }}></View>
+      }
 
-  const [hide,setHide]=useState(false);
-  const [barstyle,setBarstyle]=useState("dark-content")
-  return(
-  <View style={styles.main}>
-  <StatusBar
-   backgroundColor="orange"
-   barStyle="default"
-   
-   hidden={false}
-  />
-
-  <Button title="Toogle Status Bar" onPress={()=>setHide(!hide)}/>
-  <Button title="Update Styles" onPress={()=>setBarstyle("dark-content")}/>
-
-
-  </View>
-  )
+      <Text style= {styles.text}>Hello</Text>
+      <Text style= {styles.text}>{JSON.stringify(Platform.constants.reactNativeVersion.minor)}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
- main:{
-  flex:1,
-  justifyContent:"center",
-  backgroundColor:"black"
- }
+  main: {
+    flex: 1,
+    justifyContent:"center"
+  },
+  text:
+  {
+    color:Platform.OS=="android"?"orange":"blue" ,
+    fontSize:"30" ,
+  },
 });
 export default App;
