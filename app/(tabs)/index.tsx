42268a0c922fbd48c1201448386695f26ce58296
ChaudhariYash <yashchaudhari887@gmail.com>
2024-10-01 12:34:01 +0530

    StatusBar using styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,32 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Button, Pressable } from "react-native";
+import { Text, View, StyleSheet, Button,StatusBar} from "react-native";
 
 const App = () => {
+
+  const [hide,setHide]=useState(false);
+  const [barstyle,setBarstyle]=useState("dark-content")
   return(
   <View style={styles.main}>
-    <Pressable 
-     onPress={()=>console.warn("normal on press ")}
-     onLongPress={()=>{console.warn("long press ")}}
-    onPressIn={()=>{console.warn("press in ")}}
-    onPressOut={()=>{console.warn("press out")}}
-    >
-      <Text style={styles.PressableBtn}>Pressable </Text>
-    </Pressable>
+  <StatusBar
+   backgroundColor="orange"
+   barStyle="default"
+   
+   hidden={false}
+  />
+
+  <Button title="Toogle Status Bar" onPress={()=>setHide(!hide)}/>
+  <Button title="Update Styles" onPress={()=>setBarstyle("dark-content")}/>
+
+
   </View>
   )
 };
 
 const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    justifyContent:"center"
-  },
-  PressableBtn:
-  {
-    backgroundColor:"green",
-    color:"white",
-    padding:10,
-    margin:10,
-    borderRadius:10,
-    fontSize:20,
-    textAlign:'center',
-    shadowColor:"#000",
-    elevation:5,
-
-  },
+ main:{
+  flex:1,
+  justifyContent:"center",
+  backgroundColor:"black"
+ }
 });
 export default App;
