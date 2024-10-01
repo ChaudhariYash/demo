a25d8c654648f6bd772761d7c58cd7648167245c
ChaudhariYash <yashchaudhari887@gmail.com>
2024-10-01 18:13:33 +0530

    make custom modal and dialog box

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,47 @@
-import React  from "react";
-import{} from "react-native";
-import {WebView} from "react-native-webview"; 
+import React, { useState }  from "react";
+import{ View ,Text,StyleSheet, Button } from "react-native";
 
 const App = () => {
+  const [show,setShow]=useState(false)
   return (
-    <WebView source={{uri:"https://docs.expo.dev/versions/latest/sdk/webview/"}}/>
+     <View style={Styles.container}>
+    
+    {  show ?
+     
+      <View style={Styles.modal}>
+        <View style={Styles.body}>
+          <Text>Some text</Text>
+          <Button title="Close" onPress={()=>setShow(false)}/>
+        </View>
+      </View>:null
+    }
+   <Button title="open Dialog" onPress={()=>setShow(true)}/>
+    </View>
   )
 }
 
-
+const Styles = StyleSheet.create
+({
+  container:
+  {
+    flex:1,
+    justifyContent:"flex-end" 
+  },
+  modal:
+  {
+    flex:1,
+    backgroundColor:"rgba(50,50,50,.5)",
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  body:
+  {
+     backgroundColor:"#fff",
+     height:300,
+     width:300,
+     padding:20,
+     justifyContent:"flex-end",
+     borderRadius:10,
+  },
+})
 export default App;
