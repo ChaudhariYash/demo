f65fd0c1a8dd21f87c547071bbc50587cc264717
ChaudhariYash <yashchaudhari887@gmail.com>
2024-10-09 09:03:04 +0530

    styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.demo}>
       <Text>Home Screen</Text>
     </View>
   );
@@ -24,5 +24,13 @@ function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  demo :{
+    flex: 1,
+     alignItems: 'center',
+      justifyContent: 'center' 
+  }
+})
 
 export default App;
